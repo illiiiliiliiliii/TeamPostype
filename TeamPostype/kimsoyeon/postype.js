a7626deb113a08c75be0c06381ef2d666fc7974e
DESKTOP-IGCHC86\user <onparadox@naver.com>
2022-08-22 17:01:04 +0900

    20220822

--- TeamPostype/kimsoyeon/postype.js
+++ TeamPostype/kimsoyeon/postype.js
@@ -25,33 +25,6 @@ tab[2].addEventListener("click",function(){
     this.style.borderBottom="3px solid black";
 });
 
-const searchbox = document.querySelector("[name=searchbox]"); 
-searchbox.addEventListener("click",function(){
-    this.style.backgroundColor="white";
-});
-
-searchbox.addEventListener("mouseleave",function(){
-    this.style.backgroundColor="rgb(244, 243, 243)";
-});
-const tab =document.querySelectorAll(".HRN>div");
-tab[0].addEventListener("click",function(){
-    tab[1].style.borderBottom="none"
-    tab[2].style.borderBottom="none"
-    this.style.borderBottom="3px solid black";
-});
-
-tab[1].addEventListener("click",function(){
-    tab[0].style.borderBottom="none";
-    tab[2].style.borderBottom="none"
-    this.style.borderBottom="3px solid black";
-});
-
-tab[2].addEventListener("click",function(){
-    tab[0].style.borderBottom="none"
-    tab[1].style.borderBottom="none"
-    this.style.borderBottom="3px solid black";
-});
-
 $(document).ready(function() {
 	
 	var $slider_list = $("#slider li")
