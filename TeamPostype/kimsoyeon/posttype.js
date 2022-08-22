ac38352312e7939f7e576f95e2bc905e5e6370dd
DESKTOP-IGCHC86\user <onparadox@naver.com>
2022-08-22 11:29:58 +0900

    220822

--- TeamPostype/kimsoyeon/posttype.js
+++ TeamPostype/kimsoyeon/posttype.js
@@ -24,3 +24,46 @@ tab[2].addEventListener("click",function(){
     tab[1].style.borderBottom="none"
     this.style.borderBottom="3px solid black";
 });
+
+$(document).ready(function() {
+	
+	var $slider_list = $("#slider li")
+	console.log( $slider_list )
+	
+	$slider_list.css("left", $("#sliderbox").css("width"))
+	
+
+	$slider_list.eq(0).css("left", 0)
+	
+	var sliderClick = function() {
+		$("#sliderbox").click()
+	}
+	
+	var timerid = setInterval( sliderClick, 3000 )
+	
+	var curSlide = 0;
+	
+	$("#sliderbox").click(function() {
+// 		console.log("sliderbox click")
+
+
+	clearInterval( timerid )
+
+	timerid = setInterval( sliderClick, 3000 );
+		
+	var nextSlide = curSlide + 1;
+		nextSlide %= $slider_list.length;
+		console.log( curSlide, ":", nextSlide )
+		
+
+		$slider_list.eq(curSlide).animate( { "left": "-="+$("#sliderbox").css("width") } )
+
+		$slider_list.eq(nextSlide).css( "left", $("#sliderbox").css("width") )
+
+		$slider_list.eq(nextSlide).animate( { "left": "-="+$("#sliderbox").css("width") } )
+
+		curSlide++;
+		curSlide %= $slider_list.length;
+	})
+	
+})
